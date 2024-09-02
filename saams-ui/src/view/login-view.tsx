4088b1af3c241f66e805800e8b00620cbf8507a0
Venkata Varun  Puppala <venkata.varun.puppala@trincebio.com>
2024-09-02 13:23:27 +0200

    add dashboard view after login

--- saams-ui/src/view/login-view.tsx
+++ saams-ui/src/view/login-view.tsx
@@ -3,8 +3,6 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
@@ -12,6 +10,7 @@ import Grid from '@mui/material/Grid';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import {useState} from 'react';
 
 function Copyright(props: any) {
   return (
@@ -29,7 +28,22 @@ function Copyright(props: any) {
 // TODO remove, this demo shouldn't need to reset the theme.
 const defaultTheme = createTheme();
 
-export default function LoginView() {
+interface LoginViewProps {
+  onLogin: (userName: string, password: string) => void;
+  loginAttempted: boolean;
+}
+
+export default function LoginView({onLogin, loginAttempted} : LoginViewProps) {
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+
+  React.useEffect(() => {
+    if(loginAttempted) {
+      setUserName('');
+      setPassword('');
+    }
+  }, [loginAttempted]);
+
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
@@ -37,7 +51,23 @@ export default function LoginView() {
       email: data.get('userName'),
       password: data.get('password'),
     });
+
+    // call the onLogin function passed as a prop
+    console.log('Entered handlesubmit');
+    onLogin(userName, password);
+
+    //reset the input fields after login process
   };
+
+  const handleUserNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log("UserName: ", e.target.value);
+    setUserName(e.target.value);
+    }
+
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log("Password: ", e.target.value);
+    setPassword(e.target.value);
+  }
 
   return (
     <ThemeProvider theme={defaultTheme}>
@@ -83,6 +113,8 @@ export default function LoginView() {
                 label="User Name"
                 name="userName"
                 autoFocus
+                value={userName}
+                onChange={handleUserNameChange}
               />
               <TextField
                 margin="normal"
@@ -92,6 +124,8 @@ export default function LoginView() {
                 label="Password"
                 type="password"
                 id="password"
+                value={password}
+                onChange={handlePasswordChange}
               />
               <Button
                 type="submit"
