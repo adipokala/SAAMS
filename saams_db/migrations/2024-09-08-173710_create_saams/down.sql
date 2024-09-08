3f363fcf33c0052d5bba4680cc341434c4e5b425
Adithya Pokala <adithyapokala@outlook.com>
2024-09-08 19:49:18 +0200

    remove npm proj and add rust proj for db

---
+++ saams_db/migrations/2024-09-08-173710_create_saams/down.sql
@@ -0,0 +1,9 @@
+-- This file should undo anything in `up.sql`
+DROP TABLE IF EXISTS "roles_privileges";
+DROP TABLE IF EXISTS "companies";
+DROP TABLE IF EXISTS "designations";
+DROP TABLE IF EXISTS "shifts";
+DROP TABLE IF EXISTS "roles";
+DROP TABLE IF EXISTS "privileges";
+DROP TABLE IF EXISTS "departments";
+DROP TABLE IF EXISTS "users";
