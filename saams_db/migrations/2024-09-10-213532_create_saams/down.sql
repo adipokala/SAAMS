3b44fba50bd2c6067b4c188b971b8bf9d181687d
Adithya Pokala <adithyapokala@outlook.com>
2024-09-12 22:07:19 +0200

    update migration & create schema from sql

--- saams_db/migrations/2024-09-10-213532_create_saams/down.sql
+++ saams_db/migrations/2024-09-10-213532_create_saams/down.sql
@@ -1,9 +1,10 @@
 -- This file should undo anything in `up.sql`
+DROP TABLE IF EXISTS "users";
 DROP TABLE IF EXISTS "roles_privileges";
 DROP TABLE IF EXISTS "companies";
-DROP TABLE IF EXISTS "designations";
+DROP TABLE IF EXISTS "departments";
 DROP TABLE IF EXISTS "shifts";
+DROP TABLE IF EXISTS "designations";
 DROP TABLE IF EXISTS "roles";
 DROP TABLE IF EXISTS "privileges";
-DROP TABLE IF EXISTS "departments";
-DROP TABLE IF EXISTS "users";
+DROP TYPE IF EXISTS SEX;
