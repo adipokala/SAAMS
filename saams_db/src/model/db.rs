8010c053e24114e22f1ead0c74dccadc533b4844
Adithya Pokala <adithyapokala@outlook.com>
2024-09-14 20:21:18 +0200

    move db.rs

--- saams_db/src/model/db.rs
+++
@@ -1,7 +0,0 @@
-use diesel::prelude::*;
-use std::env;
-
-pub fn establish_connection() -> PgConnection {
-    PgConnection::establish("postgresql://saams_user:saams12345@localhost:5432/saamsdb")
-        .unwrap_or_else(|_| panic!("Error connecting to DB"))
-}
