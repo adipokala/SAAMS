c5622c4191c5e47b99f2d6f304651a900974f293
Adithya Pokala <adithyapokala@outlook.com>
2025-01-17 19:33:08 +0100

    add API endpoints for access management

---
+++ Saams.Net/Saams.Api/Models/AreaModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Saams.Api.Models
+{
+    public class AreaModel : BaseModel
+    {
+        public required string Name { get; set; }
+
+        public required string Code { get; set; }
+    }
+
+    public class AreaResponseModel : ResponseModel
+    {
+        public AreaModel? Area { get; set; }
+
+        public List<AreaModel>? Areas { get; set; }
+    }
+}
