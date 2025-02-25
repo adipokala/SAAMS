0a6bc2684d95b1392422bb729df7ffe9db182dd5
vasanthsingh <vasanthrasaputhra@gmail.com>
2025-02-25 22:25:02 +0530

    updated the code

--- saams-ui/src/global.ts
+++ saams-ui/src/global.ts
@@ -11,63 +11,64 @@ import { Channel, ChannelResponse } from "./model/channel";
 export { };
 
 declare global {
+    type NewType = {
+        loginUser: (json: string) => UserResponse;
+        // Department
+        getDepartments: () => DepartmentResponse;
+        getDepartment: (id: number) => DepartmentResponse;
+        createDepartment: (department: Department) => DepartmentResponse;
+        updateDepartment: (department: Department) => DepartmentResponse;
+        deleteDepartment: (id: number) => DepartmentResponse;
+        // Designation
+        getDesignations: () => DesignationResponse;
+        getDesignation: (id: number) => DesignationResponse;
+        createDesignation: (designation: Designation) => DesignationResponse;
+        updateDesignation: (designation: Designation) => DesignationResponse;
+        deleteDesignation: (id: number) => DesignationResponse;
+        //Privilege
+        getPrivileges: () => PrivilegeResponse;
+        getPrivilege: (id: number) => PrivilegeResponse;
+        createPrivilege: (role: Privilege) => PrivilegeResponse;
+        updatePrivilege: (role: Privilege) => PrivilegeResponse;
+        deletePrivilege: (id: number) => PrivilegeResponse;
+        // Role
+        getRoles: () => RoleResponse;
+        getRole: (id: number) => RoleResponse;
+        createRole: (role: Role) => RoleResponse;
+        updateRole: (role: Role) => RoleResponse;
+        deleteRole: (id: number) => RoleResponse;
+        // Shift
+        getShifts: () => ShiftResponse;
+        getShift: (id: number) => ShiftResponse;
+        createShift: (shift: Shift) => ShiftResponse;
+        updateShift: (shift: Shift) => ShiftResponse;
+        deleteShift: (id: number) => ShiftResponse;
+        // User
+        getUsers: () => UserResponse;
+        createUser: (user: User) => UserResponse;
+        updateUser: (user: User) => UserResponse;
+        deleteUser: (id: number) => UserResponse;
+        //Company
+        getCompanies: () => CompanyResponse;
+        getCompany: (id: number) => CompanyResponse;
+        createCompany: (company: Company) => CompanyResponse;
+        updateCompany: (company: Company) => CompanyResponse;
+        deleteCompany: (id: number) => CompanyResponse;
+        // Area
+        getAreas: () => AreaResponse;
+        getArea: (id: number) => AreaResponse; // Added method
+        createArea: (area: Area) => AreaResponse;
+        updateArea: (area: Area) => AreaResponse;
+        deleteArea: (id: number) => AreaResponse;
+        // Channel
+        getChannel: (id: number) => ChannelResponse;
+        getChannels: () => ChannelResponse;
+        createChannel: (channel: Channel) => ChannelResponse;
+        updateChannel: (channel: Channel) => ChannelResponse;
+        deleteChannel: (id: number) => ChannelResponse;
+    };
+
     interface Window {
-        electronAPI: {
-            [x: string]: any;
-            loginUser: (json: string) => UserResponse;
-            // Department
-            getDepartments: () => DepartmentResponse;
-            getDepartment: (id: number) => DepartmentResponse;
-            createDepartment: (department: Department) => DepartmentResponse;
-            updateDepartment: (department: Department) => DepartmentResponse;
-            deleteDepartment: (id: number) => DepartmentResponse;
-            // Designation
-            getDesignations: () => DesignationResponse;
-            getDesignation: (id: number) => DesignationResponse;
-            createDesignation: (designation: Designation) => DesignationResponse;
-            updateDesignation: (designation: Designation) => DesignationResponse;
-            deleteDesignation: (id: number) => DesignationResponse;
-            //Privilege
-            getPrivileges: () => PrivilegeResponse;
-            getPrivilege: (id: number) => PrivilegeResponse;
-            createPrivilege: (role: Privilege) => PrivilegeResponse;
-            updatePrivilege: (role: Privilege) => PrivilegeResponse;
-            deletePrivilege: (id: number) => PrivilegeResponse;
-            // Role
-            getRoles: () => RoleResponse;
-            getRole: (id: number) => RoleResponse;
-            createRole: (role: Role) => RoleResponse;
-            updateRole: (role: Role) => RoleResponse;
-            deleteRole: (id: number) => RoleResponse;
-            // Shift
-            getShifts: () => ShiftResponse;
-            getShift: (id: number) => ShiftResponse;
-            createShift: (shift: Shift) => ShiftResponse;
-            updateShift: (shift: Shift) => ShiftResponse;
-            deleteShift: (id: number) => ShiftResponse;
-            // User
-            getUsers: () => UserResponse;
-            createUser: (user: User) => UserResponse;
-            updateUser: (user: User) => UserResponse;
-            deleteUser: (id: number) => UserResponse;
-            //Company
-            getCompanies: () => CompanyResponse;
-            getCompany: (id: number) => CompanyResponse;
-            createCompany: (company: Company) => CompanyResponse;
-            updateCompany: (company: Company) => CompanyResponse;
-            deleteCompany: (id: number) => CompanyResponse;
-            // Area
-            getAreas: () => AreaResponse;
-            getArea: (id: number) => AreaResponse; // Added method
-            createArea: (area: Area) => AreaResponse;
-            updateArea: (area: Area) => AreaResponse;
-            deleteArea: (id: number) => AreaResponse;
-            // Channel
-            getChannel: (id: number) => ChannelResponse;
-            getChannels: () => ChannelResponse;
-            createChannel: (channel: Channel) => ChannelResponse;
-            updateChannel: (channel: Channel) => ChannelResponse;
-            deleteChannel: (id: number) => ChannelResponse;
-        }
+        electronAPI: NewType
     }
 }
